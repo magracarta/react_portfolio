60f4bfda0c0cb655a3f77d00ee75e3714ed53e88
KIM MIN JU <98890131+magracarta@users.noreply.github.com>
2024-12-27 02:47:27 +0900

    초기세팅

---
+++ portfolio/src/component/headerFooter/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header-wrap'>
+      <div className='header-top'>
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default Header
